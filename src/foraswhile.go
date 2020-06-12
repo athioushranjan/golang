379dd1992b6fba85042b36e4f7bc7622b6ebddd8
Athioush Ranjan <athioushjranjan@gmail.com>
2020-06-12 09:44:01 +0530

    first commit

---
+++ src/foraswhile.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+func main(){
+  // declaring variable to use for increment
+  i := 0
+  for i<10 {
+    //printing the i value
+    fmt.Println("incrementing the value of i", i)
+    //incrementing the variable i
+    i++
+  }
+}
